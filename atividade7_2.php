e86b705a9358d0eefa51aab4b688fc5dc1efd3ae
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 12:21:56 +0100

    Update atividade7.php and atividade7_2.php

---
+++ atividade7_2.php
@@ -0,0 +1,8 @@
+<?php
+    $nota = 12;
+    if ($nota<10)
+    {
+        echo 'Aluno <span style"color:#ff0000">Reprovado</span> com ' .$nota. ' valores';
+        echo '<br>Muito bom trabalho';
+    }
+?>
